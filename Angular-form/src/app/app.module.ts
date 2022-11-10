12ad332d1f9034a03170bf622991107408525841
Sebastian <sebasgarse@gmail.com>
2022-11-10 11:56:49 -0400

    feat: changed dir name

--- Angular-form/src/app/app.module.ts
+++ Angular-form/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormContactComponent } from './form-contact/form-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormContactComponent
   ],
   imports: [
     BrowserModule,
