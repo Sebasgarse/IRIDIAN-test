0bec6471db4a15e1c0b3d4d7f3934ac42969cefb
Sebastian <sebasgarse@gmail.com>
2022-11-10 11:59:29 -0400

    feat: added success component

--- Angular-form/src/app/app.module.ts
+++ Angular-form/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormContactComponent } from './form-contact/form-contact.component';
+import { FormSuccessComponent } from './form-success/form-success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormContactComponent
+    FormContactComponent,
+    FormSuccessComponent
   ],
   imports: [
     BrowserModule,
